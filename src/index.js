6da3c1ed6befc6bbdc910ab01b74387f5dd1fbb0
gerardmi24 <gerardmistretta@yahoo.com>
2020-10-30 10:18:06 -0400

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-let addToy = false;
-
 document.addEventListener("DOMContentLoaded", () => {
+  const baseUrl = "http://localhost:3000/toys/"
+  let addToy = false;
+  const toyDiv = document.getElementById('toy-collection')
+  
+  const fetchToys = () => {
+    fetch(baseUrl)
+    console.log(baseUrl)
+    .then(resp => resp.json())
+    .then(toys => renderToys(toys))
+    console.log(toys)
+
+    const renderToys = (toys) => {
+      for(const toy of toys){
+        renderToys(toy)
+      }
+    }
+
+    const renderToy = () => {
+
+
+
+    }
+  }
+
+
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +36,10 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
+
+
+
+
 });
